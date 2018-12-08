d2487a265ac8e9f4f3951e006b4904c933544a99
Bill Storms <bstorms@cafepress.com>
2018-12-07 22:40:22 -0500

    BoxEmUp Challenge - Took in a series of strings and created a checksum by multiplying the number of strings with at least one pair of double digts and the number of string with at least one set of triple digits

---
+++ BoxItUp/BoxItUp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BoxItUp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var codes = GetCodes();
+            var checkSum = GetCheckSum(codes);
+            Console.WriteLine(@"The checksum is {0}", checkSum);
+            Console.ReadKey();
+        }
+
+        public static List<string> GetCodes()
+        {
+            string line;
+            var codes = new List<string>();
+            var file = new StreamReader(@"C:\git\adventofcode\BoxItUp\BoxItUp\BoxIds.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                codes.Add(line);
+            }
+
+            return codes;
+
+        }
+
+        public static int GetCheckSum(List<string> codes)
+        {
+            int dubz = 0;
+            int tripz = 0;
+
+            foreach (var code in codes)
+            {
+               var doubleChars = code.GroupBy(x => x)
+                    .Where(group => @group.Count() == 2)
+                    .Select(group => @group.Key);
+
+                var tripleChars = code.GroupBy(x => x)
+                    .Where(group => @group.Count() == 3)
+                    .Select(group => @group.Key);
+
+                if (doubleChars.Any())
+                    dubz += 1;
+                if (tripleChars.Any())
+                    tripz += 1;
+            }
+
+            var checkSum = dubz * tripz;
+            return checkSum;
+        }
+    }
+}
---
+++ BoxItUp/BoxItUpTests/ProgramTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoxItUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxItUp.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        public List<string> testData = new List<string>(){"abcdefg","abcdefgga","abccdddeeffggg"};
+
+        [TestMethod()]
+        public void GetCheckSum_GoodData()
+        {
+            //Act
+            var result = Program.GetCheckSum(testData);
+
+            //Assert
+            Assert.AreEqual(result,2);
+        }
+    }
+}
